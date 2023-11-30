684b1d2dd7c0ef9816bccb8315e2683384fd2a31
Никита Клешнин <nikita.kleshnin22@yandex.ru>
2023-11-30 16:11:03 +0300

    Добавление шаблонного тега show_all_tags

--- sitedune/dune/templatetags/dune_tags.py
+++ sitedune/dune/templatetags/dune_tags.py
@@ -1,5 +1,5 @@
 from django import template
-from dune.models import Category
+from dune.models import Category, TagPost
 
 register = template.Library()
 
@@ -8,3 +8,8 @@ register = template.Library()
 def show_categories(cat_selected=0):
     cats = Category.objects.all()
     return {'cats': cats, 'cat_selected': cat_selected}
+
+
+@register.inclusion_tag('dune/list_tags.html')   # Вернет html
+def show_all_tags():
+    return {'tags': TagPost.objects.all()}

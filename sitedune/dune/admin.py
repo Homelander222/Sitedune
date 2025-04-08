d74addb9e89a609ab69e826da13380f47b8db719
Никита Клешнин <nikita.kleshnin22@yandex.ru>
2025-04-08 11:26:54 +0300

    Админ - Персонажи Дюны: 1) Доп поле - 'Краткое описание'; 2) Новые действия - 'Опубликовать выбранные записи', 'Снять с публикации выбранные записи'

--- sitedune/dune/admin.py
+++ sitedune/dune/admin.py
@@ -1,14 +1,31 @@
-from django.contrib import admin
+from django.contrib import admin, messages
+
 from .models import Dune, Category
+from django.db.models.functions import Length
 
 
 @admin.register(Dune)
 class DuneAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'time_create', 'is_published', 'cat')
-    list_display_links = ('id', 'title')
+    list_display = ('title', 'time_create', 'is_published', 'cat', 'brief_info')
+    list_display_links = ('title',)
     ordering = ('title',)
     list_editable = ('is_published', )
     list_per_page = 15
+    actions = ('set_published', 'set_draft')
+
+    @admin.display(description='Краткое описание', ordering=Length('content'))
+    def brief_info(self, dune: Dune):
+        return f"Описание {len(dune.content)} символов."
+
+    @admin.action(description='Опубликовать выбранные записи')
+    def set_published(self, request, queryset):
+        count = queryset.update(is_published=Dune.Status.PUBLISHED)
+        self.message_user(request, f'Изменено {count} записей')
+
+    @admin.action(description='Снять с публикации выбранные записи')
+    def set_draft(self, request, queryset):
+        count = queryset.update(is_published=Dune.Status.DRAFT)
+        self.message_user(request, f'{count} записей сняты с публикации!', messages.WARNING)
 
 
 @admin.register(Category)

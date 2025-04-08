4e20f084984e66791fb55820581e6ae54e38b869
Никита Клешнин <nikita.kleshnin22@yandex.ru>
2025-04-08 12:14:27 +0300

    Админ - Персонажи Дюны: 1) Подключение поиска и фильтрации по полям модели; 2) Свой фильтр PlanetFilter

--- sitedune/dune/admin.py
+++ sitedune/dune/admin.py
@@ -1,7 +1,23 @@
 from django.contrib import admin, messages
-
 from .models import Dune, Category
 from django.db.models.functions import Length
+
+
+class PlanetFilter(admin.SimpleListFilter):
+    title = 'Относится к планете'
+    parameter_name = 'planet_affiliation'
+
+    def lookups(self, request, model_admin):
+        return [
+            ('planet_yes', 'Да'),
+            ('planet_no', 'Нет')
+        ]
+
+    def queryset(self, request, queryset):
+        if self.value() == 'planet_yes':
+            return queryset.filter(planet__isnull=False)
+        elif self.value() == 'planet_no':
+            return queryset.filter(planet__isnull=True)
 
 
 @admin.register(Dune)
@@ -12,6 +28,8 @@ class DuneAdmin(admin.ModelAdmin):
     list_editable = ('is_published', )
     list_per_page = 15
     actions = ('set_published', 'set_draft')
+    search_fields = ('title__startswith', 'cat__name')
+    list_filter = ('cat__name', 'is_published', PlanetFilter)
 
     @admin.display(description='Краткое описание', ordering=Length('content'))
     def brief_info(self, dune: Dune):

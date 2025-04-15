8e2e4d71d42f8820f9221fa54fb1e73c20319c76
Никита Клешнин <nikita.kleshnin22@yandex.ru>
2025-04-15 11:58:46 +0300

    Оформление формы

--- sitedune/dune/forms.py
+++ sitedune/dune/forms.py
@@ -3,9 +3,9 @@ from .models import Category, Planet
 
 
 class AddPostForm(forms.Form):
-    title = forms.CharField(max_length=255)
-    slug = forms.SlugField(max_length=255)
-    content = forms.CharField(widget=forms.Textarea(), required=False)   # required=False - не обязательно
-    is_published = forms.BooleanField(required=False)
-    cat = forms.ModelChoiceField(queryset=Category.objects.all())
-    planet = forms.ModelChoiceField(queryset=Planet.objects.all(), required=False)
+    title = forms.CharField(max_length=255, label='Заголовок', widget=forms.TextInput(attrs={'class': 'form-input'}))  # widget - стиль
+    slug = forms.SlugField(max_length=255, label='URL')
+    content = forms.CharField(widget=forms.Textarea(attrs={'cols': 50, 'rows': 5}), required=False, label='Контент')   # required=False - не обязательно
+    is_published = forms.BooleanField(required=False, label='Статус', initial=True)
+    cat = forms.ModelChoiceField(queryset=Category.objects.all(), label='Категория', empty_label='Категория не выбрана')
+    planet = forms.ModelChoiceField(queryset=Planet.objects.all(), required=False, label='Планета', empty_label='Нет привязки к планете')

7658a33b7a4241ecb6c31a8b4616e25689c8dffc
Никита Клешнин <nikita.kleshnin22@yandex.ru>
2025-04-15 17:02:17 +0300

    Отключение локального валидатора clean_title.\

--- sitedune/dune/forms.py
+++ sitedune/dune/forms.py
@@ -42,9 +42,9 @@ class AddPostForm(forms.Form):
     planet = forms.ModelChoiceField(queryset=Planet.objects.all(), required=False, label='Планета', empty_label='Нет привязки к планете')
 
     #   Для использования в одном поле в текущей форме
-    def clean_title(self):
-        title = self.cleaned_data['title']
-        ALLOWED_CHARS = 'АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЬЫЪЭЮЯабвгдеёжзийклмнопрстуфхцчшщьыъэюя0123456789- '
-        if not (set(title) <= set(ALLOWED_CHARS)):
-            raise ValidationError('Должны присутствовать только русские символы, дефис и пробел')
+    # def clean_title(self):
+    #     title = self.cleaned_data['title']
+    #     ALLOWED_CHARS = 'АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЬЫЪЭЮЯабвгдеёжзийклмнопрстуфхцчшщьыъэюя0123456789- '
+    #     if not (set(title) <= set(ALLOWED_CHARS)):
+    #         raise ValidationError('Должны присутствовать только русские символы, дефис и пробел')
 
